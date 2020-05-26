cdd44dc54e3b594f412806d7015e71ed981b99c6
noor alam <51040978+noorshanu@users.noreply.github.com>
2020-05-26 21:48:24 +0530

    Add files via upload

---
+++ ageInDays/script.js
@@ -0,0 +1,12 @@
+function ageInDay() {
+    var birth = prompt('waht is your age?');
+    var ageInDays = (2020 - birth) * 365;
+    var h1 = document.createElement('h1');
+    var ans = document.createTextNode('you are  '+ ageInDays + ' days old');
+    h1.setAttribute('id' , 'ageinday');
+    h1.appendChild(ans);
+    document.getElementById('flex-box-result').appendChild(h1);
+}
+function reset(){
+    document.getElementById('ageinday').remove();
+}
